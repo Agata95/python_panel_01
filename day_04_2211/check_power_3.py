68aa6cbdf2babdfdc8c1d0b34c2c38c9579d0ecd
Agata Weltrowska <agata.weltrowska@ergohestia.pl>
2022-11-24 10:54:30 +0100

    Check power of 3 done

--- day_04_2211/check_power_3.py
+++ day_04_2211/check_power_3.py
@@ -1,4 +1,26 @@
+import math
+
 """
 check if datas in file are power of 3 and count it
 read numbers from file `liczby.txt` and count all of the power of 3
 """
+
+
+def is_power_of_3(number):
+    return (math.log(number) / math.log(3)) % 1 == 0
+
+
+def counting(data_int):
+    return sum([1 for num in data_int if is_power_of_3(num)])
+
+
+def open_file(path):
+    with open(path) as file:
+        number_list = file.readlines()
+    return [int(num) for num in number_list]
+
+
+list_of_num = open_file('liczby.txt')
+result = counting(list_of_num)
+
+print(f"In file are {result} numbers that are powers of 3.")
